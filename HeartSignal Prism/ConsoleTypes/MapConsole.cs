3a8141729afd73ce548a7e38f854fe1513109959
etet2 <denik302@gmail.com>
2021-08-30 23:13:36 +0100

    fix

--- HeartSignal Prism/ConsoleTypes/MapConsole.cs
+++ HeartSignal Prism/ConsoleTypes/MapConsole.cs
@@ -66,7 +66,7 @@ namespace HeartSignal
                 Program.SendNetworkMessage("southwest");
 
             }
-            else if (angle > 180 && angle < -90)
+            else if (angle > -180 && angle < -90)
             {
                 Program.SendNetworkMessage("southeast");
 
